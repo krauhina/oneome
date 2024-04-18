aa93040c432e086bb993a9f0f5fc532754354930
HRUST83 <167460554+HRUST83@users.noreply.github.com>
2024-04-18 22:14:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-im = Image.open("FOTO.jpg")
+im = Image.open("FOTO2.jpg")
 (width, height) = im.size
 height = 135
 
@@ -17,31 +17,32 @@ def cropping():
         im_crop = new_im.crop((x, 10, 100*k, 130)) #переменная содержит обрезанную фотографию буквы
         x += 100
         k += 1
+        im_crop.save(f"lol{i}.jpg")
         chebe(im_crop)
 
         if k == max:
-            return
+            break
 
 
-def chebe(im_crop):
-    im_crop.thumbnail((28, 28))
-    blackAndWhite = im_crop.convert("L")
+def chebe(img):
+    img.thumbnail((28, 28))
+    blackAndWhite = img.convert("L")
     blackAndWhite.save("bw.jpg")
     bw = Image.open("bw.jpg")
     itog = []
     x_g = []
     for y in range(28):
-        for x in range(18):
+        for x in range(19):
             color = bw.getpixel((x, y))
             if color <= 30:
                 ro = '1'
             else:
                 ro = '0'
             x_g.append(ro)
-            if x == 17:
+            if x == (18):
                 itog.append(x_g)
                 x_g = []
     os.remove("bw.jpg")
-    return itog
+    return print(itog)
 
 cropping()
