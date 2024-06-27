5868bb30530947d546bd0eb1c39d0f972f2f12bc
alkndrd <147428335+alkndrd@users.noreply.github.com>
2024-06-28 04:47:59 +0500

    Add files via upload

---
+++ final_neuro.py
@@ -0,0 +1,201 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+import cv2
+import numpy as np
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D, Dropout, Flatten, Dense, Reshape, LSTM, BatchNormalization
+from keras.optimizers import SGD, RMSprop, Adam
+import idx2numpy
+from typing import *
+import time
+
+
+# Dataset:
+# https://www.nist.gov/node/1298471/emnist-dataset
+# https://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/gzip.zip
+
+
+def cnn_print_digit(d):
+    print(d.shape)
+    for x in range(28):
+        s = ""
+        for y in range(28):
+            s += "{0:.1f} ".format(d[28*y + x])
+        print(s)
+
+
+def cnn_print_digit_2d(d):
+    print(d.shape)
+    for y in range(d.shape[0]):
+        s = ""
+        for x in range(d.shape[1]):
+            s += "{0:.1f} ".format(d[x][y])
+        print(s)
+
+
+emnist_labels = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 1025, 1105] + [i for i in range(1040, 1104)]
+
+
+def emnist_model():
+    model = Sequential()
+    model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='valid', input_shape=(28, 28, 1), activation='relu'))
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(emnist_labels), activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+    return model
+
+
+def emnist_model2():
+    model = Sequential()
+    model.add(Convolution2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Convolution2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Convolution2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(emnist_labels), activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+    return model
+
+
+def emnist_model3():
+    model = Sequential()
+    model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='same', input_shape=(28, 28, 1), activation='relu'))
+    model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(emnist_labels), activation="softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0), metrics=['accuracy'])
+    return model
+
+
+def emnist_train(model):
+    t_start = time.time()
+
+    emnist_path = "C:/PyCharm/neuro/emnist/"
+    X_train = idx2numpy.convert_from_file(emnist_path + 'emnist-byclass-train-images-idx3-ubyte')
+    y_train = idx2numpy.convert_from_file(emnist_path + 'emnist-byclass-train-labels-idx1-ubyte')
+
+    X_test = idx2numpy.convert_from_file(emnist_path + 'emnist-byclass-test-images-idx3-ubyte')
+    y_test = idx2numpy.convert_from_file(emnist_path + 'emnist-byclass-test-labels-idx1-ubyte')
+
+    X_train = np.reshape(X_train, (X_train.shape[0], 28, 28, 1))
+    X_test = np.reshape(X_test, (X_test.shape[0], 28, 28, 1))
+
+    print(X_train.shape, y_train.shape, X_test.shape, y_test.shape, len(emnist_labels))
+
+    # Test:
+    k = 10
+    X_train = X_train[:X_train.shape[0] // k]
+    y_train = y_train[:y_train.shape[0] // k]
+    X_test = X_test[:X_test.shape[0] // k]
+    y_test = y_test[:y_test.shape[0] // k]
+
+    # Normalize
+    X_train = X_train.astype(np.float32)
+    X_train /= 255.0
+    X_test = X_test.astype(np.float32)
+    X_test /= 255.0
+
+    x_train_cat = keras.utils.to_categorical(y_train, len(emnist_labels))
+    y_test_cat = keras.utils.to_categorical(y_test, len(emnist_labels))
+
+    learning_rate_reduction = keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy', patience=3, verbose=1, factor=0.5, min_lr=0.00001)
+
+    model.fit(X_train, x_train_cat, validation_data=(X_test, y_test_cat), callbacks=[learning_rate_reduction], batch_size=64, epochs=30)
+    print("Training done, dT:", time.time() - t_start)
+
+
+def emnist_predict(model, image_file):
+    img = keras.preprocessing.image.load_img(image_file, target_size=(28, 28), color_mode='grayscale')
+    emnist_predict_img(model, img)
+
+
+def emnist_predict_img(model, img):
+    img_arr = np.expand_dims(img, axis=0)
+    img_arr = 1 - img_arr/255.0
+    img_arr[0] = np.rot90(img_arr[0], 3)
+    img_arr[0] = np.fliplr(img_arr[0])
+    img_arr = img_arr.reshape((1, 28, 28, 1))
+
+    predict = model.predict([img_arr])
+    result = np.argmax(predict, axis=1)
+    return chr(emnist_labels[result[0]])
+
+
+def letters_extract(image_file: str, out_size=28):
+    img = cv2.imread(image_file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+    img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+
+    contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    output = img.copy()
+
+    letters = []
+    for idx, contour in enumerate(contours):
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if hierarchy[0][idx][3] == 0:
+            cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+            letter_crop = gray[y:y + h, x:x + w]
+
+            size_max = max(w, h)
+            letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
+            if w > h:
+                y_pos = size_max//2 - h//2
+                letter_square[y_pos:y_pos + h, 0:w] = letter_crop
+            elif w < h:
+                x_pos = size_max//2 - w//2
+                letter_square[0:h, x_pos:x_pos + w] = letter_crop
+            else:
+                letter_square = letter_crop
+
+            letters.append((x, w, cv2.resize(letter_square, (out_size, out_size), interpolation=cv2.INTER_AREA)))
+
+    letters.sort(key=lambda x: x[0], reverse=False)
+
+    return letters
+
+
+def img_to_str(model: Any, image_file: str):
+    letters = letters_extract(image_file)
+    s_out = ""
+    for i in range(len(letters)):
+        dn = letters[i+1][0] - letters[i][0] - letters[i][1] if i < len(letters) - 1 else 0
+        s_out += emnist_predict_img(model, letters[i][2])
+        if (dn > letters[i][1]/4):
+            s_out += ' '
+    return s_out
+
+
+if __name__ == "__main__":
+
+    # model = emnist_model()
+    # emnist_train(model)
+    # model.save('emnist_letters.h5')
+
+    model = keras.models.load_model('emnist_letters.h5')
+    s_out = img_to_str(model, "hi.jpg")
+    print(s_out)
+
